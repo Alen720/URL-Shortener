cbbf8fdd642873f9261aee5c2b96aff1d62b3321
Alen <alenpro720@gmail.com>
2026-08-10 06:32:50 +0400

    fix cors

--- server/app/main.py
+++ server/app/main.py
@@ -12,7 +12,7 @@ app = FastAPI(title="URL Shortener")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://url-shortener-blush-six.vercel.app/", "http://localhost:3000", "http://127.0.0.1:3000"],
+    allow_origins=["https://url-shortener-blush-six.vercel.app", "http://localhost:3000", "http://127.0.0.1:3000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"]
