22d98b1084940740ea8e63458305893b916c4dcf
Alen <alenpro720@gmail.com>
2026-08-12 04:16:12 +0400

    fix loading spiner

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -87,13 +87,11 @@ function App() {
 
       {loading && (
         <div className="loading-status">
-          <div className="loading-status">
-            <p className="loading-text">
-              ⏳ Shortening the link... The server may take up to 30–40 seconds
-              to wake up.
-            </p>
-            <div className="loading-spinner"></div>
-          </div>
+          <p className="loading-text">
+            ⏳ Shortening the link... The server may take up to 30–40 seconds
+            to wake up.
+          </p>
+          <div className="loading-spinner"></div>
         </div>
       )}
 
